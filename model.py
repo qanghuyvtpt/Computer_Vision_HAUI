1a69bdb739354617095b9253ee3d6f606fe133e2
quang huy <qanghuyvtpt@gmail.com>
2026-05-15 11:50:43 +0700

    update

--- model.py
+++ model.py
@@ -1,16 +1,16 @@
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
 import torch.nn as nn
 from torchsummary import summary
 import torch
 
-class my_resnet(nn.Module):
+class my_model(nn.Module):
     def __init__(self, num_classes = 3):
         super().__init__()
-        self.backbone = resnet50(weights = ResNet50_Weights.DEFAULT)
-        self.backbone.fc = nn.Identity()    # bo classifier
+        self.backbone = mobilenet_v2(weights=MobileNet_V2_Weights.IMAGENET1K_V1)
+        self.backbone.classifier = nn.Identity()   # bo classifier
         self.head = nn.Sequential(
             nn.Dropout(0.3),
-            nn.Linear(2048, 256),
+            nn.Linear(1280, 256),
             nn.ReLU(),
             nn.BatchNorm1d(256),
             nn.Dropout(0.2),
@@ -27,7 +27,7 @@ class my_resnet(nn.Module):
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = my_resnet().to(device)
+    model = my_model().to(device)
 
 
     summary(model, input_size=(3,224,224))
--- train_model.py
+++ train_model.py
@@ -1,16 +1,16 @@
-from model import my_resnet
+from model import my_model
 from dataset import AnimalDataset
 import torch
 from argparse import ArgumentParser
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch.nn as nn
-from torchvision.transforms import Compose, Resize, ToTensor
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize, RandomAffine, ColorJitter
 
 
 def get_args():
     parser = ArgumentParser("animal classifier")
-    parser.add_argument("--batch_size", "-b", type = int, default=16)
+    parser.add_argument("--batch_size", "-b", type = int, default=128)
     parser.add_argument("--epochs", "-e", type = int, default=30)
     parser.add_argument("--logging", "-l",type = str, default="./tensorboard" )
     parser.add_argument("--train_model", "-t",type = str, default="./train_model" )
@@ -26,7 +26,23 @@ def train():
 
     transform = Compose([
         Resize((224, 224)),
+        RandomAffine(
+            degrees=21,
+            translate=(0.05, 0.05),
+            scale=(0.8, 1.2),
+            shear=0.7
+        ),
+        ColorJitter(
+            brightness=0.24,
+            contrast=0.5,
+            saturation=0.25,
+            hue=0.05
+        ),
         ToTensor(),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
     ])
 
     train_dataset = AnimalDataset(root=r"C:\Users\Admin\Desktop\ki6\xu_ly_anh_so", train=True, transform=transform)
@@ -37,14 +53,23 @@ def train():
         drop_last=True
     )
 
-    model = my_resnet().to(device)
+    model = my_model().to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(params= model.parameters())
 
-    for images, labels in train_dataloader:
-        print(images.shape)
-        print(labels)
+    num_iters = len(train_dataloader)
 
+    for epoch in range(num_epochs):
+        model.train()
+        progress_bar = tqdm(train_dataloader, colour='green')
+        for iter,  (images, labels) in enumerate(progress_bar):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            #forward
+            outputs = model(images)
+            loss_value = criterion(outputs, labels)
+            progress_bar.set_description(f'epoch {epoch+1}/{num_epochs}.Iteration {iter+1}/{num_iters}. Loss= {loss_value:.3f} ')
 
 
 if __name__ == '__main__':
